2a3daa0ac9690698f96c6f29eab1f44e0972c935
Wasim Rehman <iam_wasim@outlook.com>
2022-11-23 15:00:43 +0530

    press home page added

--- components/footer.js
+++ components/footer.js
@@ -1,24 +1,30 @@
 export const footer = () => {
     return `
-        <div class="new_footer_top">
-        <div class="links">
-          <a href="#">jhcdj</a>
-        </div>
-          <div class="footer_bg">
-            <div class="footer_bg_one"></div>
-            <div class="footer_bg_two"></div>
-          </div>
-      </div>
-      <div class="footer_bottom">
-          <div class="container">
-            <div class="row align-items-center">
-              <div class="col-lg-6 col-sm-7">
-                <p class="mb-0 f_400">
-                    Copyright © Vikas Bansal 2022 All rights reserved.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+			<div class="footer-right">
+
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-linkedin"></i></a>
+				<a href="#"><i class="fa fa-github"></i></a>
+
+			</div>
+
+			<div class="footer-left">
+
+				<p class="footer-links">
+					<a class="link-1" href="/">Home</a>
+
+					<a href="/about.html">About</a>
+
+					<a href="/books.html">Books</a>
+
+					<a href="/press.html">Press</a>
+
+					<a href="/contact.html">Contact</a>
+				</p>
+
+				<p>Copyright &copy; Vikas Bansal 2022</p>
+				<p>Developed by <a class="dev" href="https://github.com/WasimRehmn">Wasim Rehman</a></p>
+			</div>
     `
 }
